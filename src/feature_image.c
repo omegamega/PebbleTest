5bb5e0dc7951f41b3f4619a02f64e2802c36a79c
omega <omegamega486@gmail.com>
2014-10-06 03:42:15 +0900

    Add feature that tap-action show clock

--- src/feature_image.c
+++ src/feature_image.c
@@ -1,40 +1,50 @@
-/*
-
-   Demonstrate how to display a two color, black and white bitmap
-   image with no transparency.
-
-   The original source image is from:
-
-      <http://openclipart.org/detail/26728/aiga-litter-disposal-by-anonymous>
-
-   The source image was converted from an SVG into a RGB bitmap using
-   Inkscape. It has no transparency and uses only black and white as
-   colors.
-
- */
-
 #include "pebble.h"
 
 static Window *window;
 
 static BitmapLayer *image_layer;
+static BitmapLayer *image_layer2;
+
+static TextLayer *text_layer;
 
 static GBitmap *image;
 static GBitmap *image2;
 
-static AppTimer *timer;
-static int count = 0;
+// handle Tick, and flash bg
+void tick_handler(struct tm *tick_time, TimeUnits units_changed)
+{
+	if(layer_get_hidden((Layer*)text_layer) == false)
+	{
+		//Allocate long-lived storage (required by TextLayer)
+		static char buffer[] = "00:00";
 
-void timer_callback(void *data){
-	count++;
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "count %d", count);
-	
-	if(count % 2) {
-		bitmap_layer_set_bitmap(image_layer, image);
-	}else{
-		bitmap_layer_set_bitmap(image_layer, image2);
+		//Write the time to the buffer in a safe manner
+		strftime(buffer, sizeof(buffer), "%H:%M", tick_time);
+
+		//Set the TextLayer to display the buffer
+		text_layer_set_text(text_layer, buffer);
 	}
-	timer = app_timer_register(500, timer_callback, NULL);
+	else
+	{
+		if(layer_get_hidden((Layer*)image_layer2) == false){
+			layer_set_hidden((Layer*)image_layer2, true);
+		}else{
+			layer_set_hidden((Layer*)image_layer2, false);	
+		}
+	}
+}
+
+// handle tap, and show number face 
+void tap_hide_text_timer_callback(void* data) {
+	layer_set_hidden((Layer*)text_layer,true);
+	light_enable(false);
+}
+
+void tap_handler(AccelAxisType axis, int32_t direction)
+{
+	layer_set_hidden((Layer*)text_layer, false);
+	light_enable(true);
+	app_timer_register(3000, tap_hide_text_timer_callback, NULL);	
 }
 
 int main(void) {
@@ -55,7 +65,25 @@ int main(void) {
   bitmap_layer_set_alignment(image_layer, GAlignCenter);
   layer_add_child(window_layer, bitmap_layer_get_layer(image_layer));
 	
-	timer = app_timer_register(500, timer_callback, NULL);
+  image_layer2 = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(image_layer2, image2);
+  bitmap_layer_set_alignment(image_layer2, GAlignCenter);
+  layer_add_child(window_layer, bitmap_layer_get_layer(image_layer2));
+	
+  //Create the TextLayer
+  text_layer = text_layer_create(GRect(0, 0, 144, 50));
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlack);
+
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+  text_layer_set_text_alignment(text_layer, GTextAlignmentLeft);
+	layer_set_hidden((Layer*)text_layer, true);
+
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+	
+	// set timer
+	tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+	accel_tap_service_subscribe(tap_handler);
 	
   app_event_loop();
 
@@ -63,5 +91,6 @@ int main(void) {
   gbitmap_destroy(image2);
 
   bitmap_layer_destroy(image_layer);
+	text_layer_destroy(text_layer);
   window_destroy(window);
 }
